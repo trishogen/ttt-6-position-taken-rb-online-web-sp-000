bdb2db63b6d9e78f5c0f1e224d5ec751300231b2
Trish Ogen <patriciaogen@gmail.com>
2019-05-09 01:55:54 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,8 @@
 def position_taken?(board, index)
   if board[index - 1] == " " || board[index - 1] == "" || board[index - 1] == nil
     false
+  elsif board[index - 1] == "X" || board[index - 1] == "O"
+    true
   end
 end
   
